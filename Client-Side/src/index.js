6ae3d864c33a62551dde9488d2f4a80ba48aedcb
mdnahid47 <nmd97243@gmail.com>
2026-06-30 02:44:23 +0600

    debug

--- Client-Side/src/index.js
+++ Client-Side/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Adjust this if you don’t have a CSS file
 import App from './App.jsx';
-
+console.log('🔍 Environment Check:');
+console.log('Mode:', import.meta.env.MODE);
+console.log('API URL:', import.meta.env.VITE_API_URL);
+console.log('All env:', import.meta.env);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
